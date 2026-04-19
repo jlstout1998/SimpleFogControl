9a2dbb42225abcfc183b16d1d8351d3e8b349803
Manuel Kasten <kasten.m@gmx.de>
2026-04-19 19:11:31 +0200

    rebuild rain fog calculation method
    fixes:
    - missing clouds for small render distances or small fog end values
    - fog distances being applied slowly after settings change (now instant)

--- common/src/main/java/de/draradech/simplefog/mixin/AtmosphericFogEnvironmentMixin.java
+++ common/src/main/java/de/draradech/simplefog/mixin/AtmosphericFogEnvironmentMixin.java
@@ -9,6 +9,7 @@ import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.fog.FogData;
 import net.minecraft.client.renderer.fog.environment.AtmosphericFogEnvironment;
 import net.minecraft.core.BlockPos;
+import net.minecraft.util.Mth;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.levelgen.Heightmap;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,8 +19,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AtmosphericFogEnvironment.class)
 public class AtmosphericFogEnvironmentMixin {
-    private static float currentFogStartPercent = Float.NaN;
-    private static float currentFogEndPercent = Float.NaN;
+    private static float currentRainFactor = 0.0f;
+    private static float currentIndoorFactor = 0.0f;
     private static float approach(float current, float target, float step) {
         if (Float.isNaN(current)) return target;
         return current < target ? Math.min(target, current + step) : Math.max(target, current - step);
@@ -28,42 +29,47 @@ public class AtmosphericFogEnvironmentMixin {
     @Inject(at = @At("TAIL"), method = "setupFog(Lnet/minecraft/client/renderer/fog/FogData;Lnet/minecraft/client/Camera;Lnet/minecraft/client/multiplayer/ClientLevel;FLnet/minecraft/client/DeltaTracker;)V")
     public void tailSetupFog(FogData fogData, Camera camera, ClientLevel clientLevel, float viewDistance, DeltaTracker deltaTracker, CallbackInfo ci)
     {
-        if (SimpleFogMain.config.overworldToggle && clientLevel.dimension() == Level.OVERWORLD)
+        SimpleFogConfig config = SimpleFogMain.config;
+        if (config.overworldToggle && clientLevel.dimension() == Level.OVERWORLD)
         {
-            float targetFogStartPercent = SimpleFogMain.config.overworldStart;
-            float targetFogEndPercent = SimpleFogMain.config.overworldEnd;
-            SimpleFogConfig.RainConfig rainConf = SimpleFogMain.config.rainConfig;
+            SimpleFogConfig.RainConfig rainConf = config.rainConfig;
 
-            if (rainConf.rainToggle && clientLevel.isRaining()) {
+            if (rainConf.rainToggle)
+            {
                 BlockPos blockPos = camera.blockPosition();
                 boolean skylight = blockPos.getY() >= clientLevel.getHeight(Heightmap.Types.WORLD_SURFACE, blockPos.getX(), blockPos.getZ());
-                targetFogStartPercent = skylight ? rainConf.rainStart : rainConf.rainStartIndoor;
-                targetFogEndPercent = rainConf.rainEnd;
-            }
+                float step = rainConf.rainFogApplySpeed * deltaTracker.getRealtimeDeltaTicks() * 0.01f;
+                currentRainFactor = approach(currentRainFactor, clientLevel.isRaining() ? 1.0f : 0.0f, step);
+                currentIndoorFactor = approach(currentIndoorFactor, skylight ? 0.0f : 1.0f, step);
 
-            if (currentFogStartPercent != targetFogStartPercent || currentFogEndPercent != targetFogEndPercent) {
-                float applySpeed = rainConf.rainFogApplySpeed * deltaTracker.getRealtimeDeltaTicks();
-                currentFogStartPercent = approach(currentFogStartPercent, targetFogStartPercent, applySpeed);
-                currentFogEndPercent = approach(currentFogEndPercent, targetFogEndPercent, applySpeed);
+                float rainStartPerc = Mth.lerp(currentIndoorFactor, rainConf.rainStart, rainConf.rainStartIndoor);
+                float envStartPerc = Mth.lerp(currentRainFactor, config.overworldStart, rainStartPerc);
+                float envEndPerc = Mth.lerp(currentRainFactor, config.overworldEnd, rainConf.rainEnd);
+                float cloudEndClear = Minecraft.getInstance().options.cloudRange().get() * 16.0f;
+                float cloudEndRain = cloudEndClear * rainConf.rainEnd / config.overworldEnd;
+                fogData.environmentalStart = viewDistance * envStartPerc * 0.01f;
+                fogData.environmentalEnd = viewDistance * envEndPerc * 0.01f;
+                fogData.cloudEnd = Mth.lerp(currentRainFactor, cloudEndClear, cloudEndRain);
             }
-
-            fogData.environmentalStart = viewDistance * currentFogStartPercent * 0.01f;
-            fogData.environmentalEnd = viewDistance * currentFogEndPercent * 0.01f;
+            else
+            {
+                fogData.environmentalStart = viewDistance * config.overworldStart * 0.01f;
+                fogData.environmentalEnd = viewDistance * config.overworldEnd * 0.01f;
+                fogData.cloudEnd = Minecraft.getInstance().options.cloudRange().get() * 16.0f;
+            }
             fogData.skyEnd = Math.min(fogData.environmentalEnd, viewDistance);
-            float cloudEndClear = Minecraft.getInstance().options.cloudRange().get() * 16.0f;
-            fogData.cloudEnd = rainConf.rainEnd + (cloudEndClear - rainConf.rainEnd) * ((fogData.environmentalEnd - rainConf.rainEnd) / Math.max(viewDistance * SimpleFogMain.config.overworldEnd * 0.015f - rainConf.rainEnd, 1.0f));
         }
-        else if (SimpleFogMain.config.netherToggle && clientLevel.dimension() == Level.NETHER)
+        else if (config.netherToggle && clientLevel.dimension() == Level.NETHER)
         {
-            fogData.environmentalStart = viewDistance * SimpleFogMain.config.netherStart * 0.01f;
-            fogData.environmentalEnd = viewDistance * SimpleFogMain.config.netherEnd * 0.01f;
+            fogData.environmentalStart = viewDistance * config.netherStart * 0.01f;
+            fogData.environmentalEnd = viewDistance * config.netherEnd * 0.01f;
             fogData.cloudEnd = fogData.environmentalEnd;
             fogData.skyEnd = fogData.environmentalEnd;
         }
-        else if (SimpleFogMain.config.endToggle && clientLevel.dimension() == Level.END)
+        else if (config.endToggle && clientLevel.dimension() == Level.END)
         {
-            fogData.environmentalStart = viewDistance * SimpleFogMain.config.endStart * 0.01f;
-            fogData.environmentalEnd = viewDistance * SimpleFogMain.config.endEnd * 0.01f;
+            fogData.environmentalStart = viewDistance * config.endStart * 0.01f;
+            fogData.environmentalEnd = viewDistance * config.endEnd * 0.01f;
             fogData.cloudEnd = fogData.environmentalEnd;
             fogData.skyEnd = fogData.environmentalEnd;
         }
