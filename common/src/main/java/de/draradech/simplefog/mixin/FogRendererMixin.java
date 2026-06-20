fb63a0334f95ace4dc4c235ce33b2f8c29cdd209
jlstout1998 <jlstout1998@gmail.com>
2026-06-19 21:24:45 -0700

    Fix syntax error in active method condition

--- common/src/main/java/de/draradech/simplefog/mixin/FogRendererMixin.java
+++ common/src/main/java/de/draradech/simplefog/mixin/FogRendererMixin.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public class FogRendererMixin {
     private boolean active()
     {
-        if (!SimpleFogMain.config.overworldToggle && Minecraft.getInstance()player..level().dimension() == Level.OVERWORLD) return false;
+        if (!SimpleFogMain.config.overworldToggle && Minecraft.getInstance().player.level().dimension() == Level.OVERWORLD) return false;
         if (!SimpleFogMain.config.netherToggle && Minecraft.getInstance().player.level().dimension() == Level.NETHER) return false;
         if (!SimpleFogMain.config.endToggle && Minecraft.getInstance().player.level().dimension() == Level.END) return false;
         return true;
